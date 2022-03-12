cbe0f25a0c35b565f5eacb99ba60f86c6d637da1
Nguyen Ich Phuoc <phuoc.nguyenich@mcpayment.com>
2022-03-12 16:35:18 +0700

    feat: domain area

---
+++ Ich.CQRS.Core/Ich.CQRS.Core/Domain/Booking.partial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ich.CQRS.Core.Domain
+{
+    public partial class Booking
+    {
+        public bool ShouldSerializeTraveler()
+        {
+            return false;
+        }
+        public bool ShouldSerializeSeat()
+        {
+            return false;
+        }
+        public bool ShouldSerializeFlight() 
+        { 
+            return false; 
+        }
+    }
+}
---
+++ Ich.CQRS.Core/Ich.CQRS.Core/Domain/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ich.CQRS.Core.Domain
+{
+    public partial class Event
+    {
+        public int Id { get; set; }
+        public string Transaction { get; set; }
+        public DateTime EventDate { get; set; }
+        public string Action { get; set; }
+        public string Table { get; set; }
+        public int TableId { get; set; }
+        public int Version { get; set; }
+        public string Content { get; set; }
+    }
+}
