c7addfe19692fe6bd9b41736ed52cdf6454e32a3
Nguyen Ich Phuoc <phuoc.nguyenich@mcpayment.com>
2022-03-12 18:11:57 +0700

    feat: home cache attribute

---
+++ Ich.CQRS.Core/Ich.CQRS.Core/Areas/Home/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Ich.CQRS.Core.Code.Attributes;
+using Ich.CQRS.Core.Code.Caching;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ich.CQRS.Core.Areas.Home
+{
+    [Menu("Welcome")]
+    public class HomeController : Controller
+    {
+        public HomeController(ICache cache)
+        { }
+
+        [HttpGet("")]
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
---
+++ Ich.CQRS.Core/Ich.CQRS.Core/Code/Attributes/MenuAttribute.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Ich.CQRS.Core.Code.Attributes
+{
+    public class MenuAttribute : ActionFilterAttribute
+    {
+        private readonly string _menu;
+
+        public MenuAttribute(string menu)
+        {
+            _menu = menu;
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            (filterContext.Controller as Controller).ViewBag.Menu = _menu;
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
