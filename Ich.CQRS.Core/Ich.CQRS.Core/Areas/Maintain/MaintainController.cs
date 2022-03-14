8070c2565ac51628c798e605e55691acb41d5fbd
Nguyen Ich Phuoc <phuoc.nguyenich@mcpayment.com>
2022-03-14 16:30:15 +0700

    feat: seat maintain

---
+++ Ich.CQRS.Core/Ich.CQRS.Core/Areas/Maintain/MaintainController.cs
@@ -0,0 +1,39 @@
+﻿using Ich.CQRS.Core.Code.Attributes;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Ich.CQRS.Core.Areas.Maintain
+{
+    [Menu("Admin")]
+    [AdminMenu("Maintain")]
+    [Route("admin/maintain")]
+    public class MaintainController : Controller
+    {
+        private readonly IMediator _mediator;
+
+        public MaintainController(IMediator mediator) { _mediator = mediator; }
+
+        #region Queries
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(Edit.Query query)
+        {
+            var model = await _mediator.Send(query);
+            return View(model);
+        }
+
+        #endregion
+
+        #region Commands
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Edit.Command command)
+        {
+            await _mediator.Send(command);
+            return View(command);
+        }
+
+        #endregion
+    }
+}
